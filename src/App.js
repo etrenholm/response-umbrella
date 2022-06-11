d0bbd05cb5d8277faa1e3c6624fdaa035ffbefd2
Erica <erica.trenholm48@gmail.com>
2022-06-11 13:27:35 -0400

    fixed margin

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from 'react';
+import React, { useState } from 'react';
 import Header from './components/Header';
 import Footer from './components/Footer';
 import Resume from './pages/Resume';
