6305456bebc381488b2cbcba9eefdaa2ba04fe03
Erica <erica.trenholm48@gmail.com>
2022-09-10 14:32:44 -0400

    updated resume and about

--- src/pages/About.js
+++ src/pages/About.js
@@ -12,7 +12,8 @@ function About() {
 
             <div>
             <h2>About</h2>
-            <p>I’m a graphic designer and aspiring front-end web developer, passionate about utilizing the right-brain and left-brain to develop logical, structural and creative design.</p>
+            <p>I’m Front-end Web Developer with a background in Graphic Design and a keen eye for detail, effective at combining both sides of the brain to develop logical, creative and user-friendly designs and applications. I'm also known for having a strong work ethic and exceptional organizational skills.</p>
+            <p>Currently, I work as a Graphic Designer at Spitfire Communications in Simsbury, Connecticut. I've recently earned a Certificate in Full Stack Web Development from the University of Connecticut and am eager to strengthen my coding skills.</p>
             <p>I was born and raised in Manchester, Connecticut until I attended Roger Williams University in Bristol, Rhode Island. I graduated in 2015 with a double major in Graphic Design and Music and a minor in Marketing.</p>
             <p>In my free time I enjoy spending time with my friends and family, running, hiking and just generally being active. By far, my biggest passion is music. I can usually be found singing. Throughout my life, I have been a member of multiple choral ensembles and also received my second degree in music just for fun.</p>
             <p>Currently, I live in Farmington, Connecticut and work as a Graphic Designer for Spitfire Communications in Simsbury.</p>
